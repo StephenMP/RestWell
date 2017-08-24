8b6f1574582c5827d1eb15d03d7d59d069a39764
StephenMP <1.stephen.porter@gmail.com>
2017-08-24 11:43:35 -0600

    Quick bug fix

--- RestWell/Client/Proxy.cs
+++ RestWell/Client/Proxy.cs
@@ -31,7 +31,7 @@ namespace RestWell.Client
         public Proxy(IProxyConfiguration proxyConfiguration)
         {
             this.proxyConfiguration = proxyConfiguration ?? new ProxyConfiguration();
-            this.httpClient = HttpClientFactory.Create(proxyConfiguration.DelegatingHandlers.Values);
+            this.httpClient = HttpClientFactory.Create(this.proxyConfiguration.DelegatingHandlers.Values);
         }
 
         #endregion Public Constructors
